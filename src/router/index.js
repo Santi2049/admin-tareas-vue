68f1ca7976d43caa6ae210fbbdc48d693c435892
Santiago Magaña <143046223+Santi2049@users.noreply.github.com>
2024-01-15 16:25:10 -0600

    Se crea el archivo router para las rutas del proyecto y se instalan las dependencias necesarias

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Inicio from '../views/TareasView.vue'
+import { create } from "core-js/core/object";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'listado-tareas',
+            component: Inicio,
+            props: { titulo: 'Listado de Tareas' }
+        },
+        {
+            path: '/agregar-tarea',
+            name: 'agregar-tarea',
+            component: () => import('../views/NuevaTareaView.vue'),
+            props: { titulo: 'Agregar Tarea' }
+        },
+        {
+            path: '/editar-tarea/:id',
+            name: 'editar-tarea',
+            component: () => import('../views/EditarTareaView.vue'),
+            props: { titulo: 'Editar Tarea' }
+        },
+        {
+            path: '/iniciar-sesion',
+            name: 'iniciar-sesion',
+            component: () => import('../views/LoginView.vue'),
+            props: { titulo: 'Iniciar Sesión' }
+        },
+        {
+            path: '/registrate',
+            name: 'registrate',
+            component: () => import('../views/RegistroView.vue'),
+            props: { titulo: 'Registrate' }
+        },
+
+
+    ]
+})
+
+export default router

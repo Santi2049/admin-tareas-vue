f6907237e595e75079592c6e84766a7a115fedc1
Santiago Magaña <143046223+Santi2049@users.noreply.github.com>
2024-01-15 19:23:04 -0600

    Se añade el formulario para agragar tareas

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { plugin, defaultConfig } from '@formkit/vue'
+//import config from '../formkit.config'
 import './assets/main.css'
 
 const app = createApp(App)
 app.use(router)
+app.use(plugin, defaultConfig())
 
 
 
--- src/views/NuevaTareaView.vue
+++ src/views/NuevaTareaView.vue
@@ -1,13 +1,85 @@
+<script setup>
+import RouterLink from '../components/UI/RouterLink.vue'
+import HeadingComp from '../components/UI/HeadingComp.vue'
+</script>
+const selectOptions = [
+  {value: 'Prioritaria', label: 'Prioritaria'},
+  {value: ' No Prioritaria', label: 'No Prioritaria'},
+]
 <template>
     <div>
+        <div class="flex justify-end">
+      <RouterLink to="listado-tareas">
+        Volver
+      </RouterLink>
+    </div>
 
+    <HeadingComp>{{ titulo }}</HeadingComp>
+
+    <div class="mx-auto mt-10 bg-white shadow">
+      <div class="mx-auto md:w-2/3 py-20 px-6">
+        <FormKit
+            type="form"
+            submit-label="Agregar Tarea"
+            incomplete-message="No se pudo enviar, revisa los mensajes"
+            @submit="handleSubmit"
+        >
+          <FormKit
+              type="text"
+              label="Título"
+              name="titulo"
+              placeholder="Título de Tarea"
+              validation="required"
+              :validation-messages="{ required: 'El título de la tarea es Obligatorio'}"
+          />
+
+          <FormKit
+              type="text"
+              label="Materia"
+              name="materia"
+              placeholder="Nombre de a Materia"
+              validation="required"
+              :validation-messages="{ required: 'El título de la Materia es Obligatorio'}"
+          />
+
+          <FormKit
+              type="select"
+              label="Prioridad"
+              :options="selectOptions"
+              name="prioridad"
+              placeholder="Nivel de Prioridad"
+              validation="required"
+              :validation-messages="{ required: 'El Nivel de Prioridad es Obligatorio'}"
+          />
+
+          <FormKit
+              type="date"
+              label="Fecha de entrega"
+              name="fecha"
+              validation="date_after:2024-01-12"
+              :validation-messages="{ date_after: 'La fecha no puede ser anterior a la actual'}"
+              v-model="selectedDate"
+          />
+
+          <FormKit
+              type="textarea"
+              label="Especificaciones"
+              name="specs"
+              placeholder="Especificaciones de la Tarea"
+              validation="required"
+              :validation-messages="{ required: 'Las especificaciones de la tarea son Obligatorias'}"
+          />
+        </FormKit>
+      </div>
+</div>
     </div>
 </template>
 
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
+<style>
+.formkit-wrapper {
+  max-width: 100%;
+}
 </style>
+
+
+

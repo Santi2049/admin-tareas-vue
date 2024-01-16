68865d37590dd4470d0230cdc67ef2397c700450
José Enrique Ortega Valladares <130116938+Ortegavalladares@users.noreply.github.com>
2024-01-16 00:08:43 -0600

    se hace la estructura de la vista para editar tarea y se crea el archivo de la base que se cargara a json server

--- src/views/EditarTareaView.vue
+++ src/views/EditarTareaView.vue
@@ -1,13 +1,110 @@
-<template>
-    <div>
-
-    </div>
-</template>
-
 <script setup>
+import { FormKit } from '@formkit/vue'
+import RouterLink from '../components/UI/RouterLink.vue'
+import HeadingComp from '../components/UI/HeadingComp.vue'
+import { defineProps, watch, ref  } from 'vue'
+
+defineProps({
+  titulo: {
+    type: String
+  }
+})
+
+const currentDate = ref(getCurrentDate());
+const selectedDate = ref(getCurrentDate());
+
+function getCurrentDate() {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); // Enero es 0!
+  const yyyy = today.getFullYear();
+
+  return `${yyyy}-${mm}-${dd}`;
+}
+
+watch(currentDate, () => {
+  // Actualiza el valor de selectedDate cuando currentDate cambia
+  selectedDate.value = getCurrentDate();
+});
+
+const selectOptions = [
+  {value: 'Prioritaria', label: 'Prioritaria'},
+  {value: ' No Prioritaria', label: 'No Prioritaria'},
+]
 
 </script>
 
-<style lang="scss" scoped>
+<template>
+    <div>
+        <div class="flex justify-end">
+      <RouterLink to="listado-tareas">
+        Volver
+      </RouterLink>
+    </div>
 
+    <HeadingComp>{{ titulo }}</HeadingComp>
+
+    <div class="mx-auto mt-10 bg-white shadow">
+      <div class="mx-auto md:w-2/3 py-20 px-6">
+        <FormKit
+            type="form"
+            submit-label="Agregar Tarea"
+            incomplete-message="No se pudo enviar, revisa los mensajes"
+            
+        >
+          <FormKit
+              type="text"
+              label="Título"
+              name="titulo"
+              placeholder="Título de Tarea"
+              validation="required"
+              :validation-messages="{ required: 'El título de la tarea es Obligatorio'}"
+          />
+
+          <FormKit
+              type="text"
+              label="Materia"
+              name="materia"
+              placeholder="Nombre de a Materia"
+              validation="required"
+              :validation-messages="{ required: 'El título de la Materia es Obligatorio'}"
+          />
+
+          <FormKit
+              type="select"
+              label="Prioridad"
+              :options="selectOptions"
+              name="prioridad"
+              placeholder="Nivel de Prioridad"
+              validation="required"
+              :validation-messages="{ required: 'El Nivel de Prioridad es Obligatorio'}"
+          />
+
+          <FormKit
+              type="date"
+              label="Fecha de entrega"
+              name="fecha"
+              validation="date_after:2024-01-15"
+              :validation-messages="{ date_after: 'La fecha no puede ser anterior a la actual'}"
+              v-model="selectedDate"
+          />
+
+          <FormKit
+              type="textarea"
+              label="Especificaciones"
+              name="specs"
+              placeholder="Especificaciones de la Tarea"
+              validation="required"
+              :validation-messages="{ required: 'Las especificaciones de la tarea son Obligatorias'}"
+          />
+        </FormKit>
+      </div>
+</div>
+    </div>
+</template>
+
+<style>
+.formkit-wrapper {
+  max-width: 100%;
+}
 </style>
